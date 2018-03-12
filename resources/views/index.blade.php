97ab2e24fed294717deae4525f655ee2b3c55e53
lebaotrung <trung1995bao@gmail.com>
2018-03-12 13:56:31 +0700

    fix css link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -176,7 +176,7 @@
 
         <!-- Footer -->
         <footer class="text-center">
-             <span>Contact</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="mailto:hello@morphehome.com"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp;<a href="https://www.facebook.com/MorpheExperience/" target="_blank" title=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+             <span>Contact us</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="fa fa-envelope-o" href="mailto:hello@morphehome.com"></a>&nbsp;&nbsp;&nbsp;<a class="fa fa-facebook" href="https://www.facebook.com/MorpheExperience/" target="_blank" title=""></a>
         </footer>
         <!-- ./Footer -->
     <script src="{{ asset('assets/dist/app.js') }}"></script>
